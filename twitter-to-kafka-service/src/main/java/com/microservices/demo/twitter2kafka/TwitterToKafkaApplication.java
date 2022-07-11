b956bd3afddfbc07bd39fe30a8daab440df2b9fa
abuazlan <abid.linux@gmail.com>
2022-07-11 20:48:04 +0530

    added twitter to kafka service. just the skeleton

---
+++ twitter-to-kafka-service/src/main/java/com/microservices/demo/twitter2kafka/TwitterToKafkaApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.demo.twitter2kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TwitterToKafkaApplication.class, args);
+	}
+
+}
---
+++ twitter-to-kafka-service/src/test/java/com/microservices/demo/twitter2kafka/TwitterToKafkaApplicationTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.microservices.demo.twitter2kafka;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author abids
+ *
+ */
+class TwitterToKafkaApplicationTest {
+
+	@Test
+	void contextLoad() {
+	}
+
+}
